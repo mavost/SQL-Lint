04ff6906d0c6b9f0986ff27da244e478addef1e4
Markus von Steht <markus.von.steht@gmx.de>
2025-09-21 15:47:30 +0200

    base case for test

--- sql/test.sql
+++ sql/test.sql
@@ -1,7 +1,12 @@
-WITH cte AS (
-    SELECT id, Name FROM Users u
-    JOIN Roles r ON u.roleId = r.id
+
+USE SPOT;
+
+GO
+
+WITH [cte] AS (
+    SELECT u.[id], r.[Name] FROM [Users] u
+    JOIN [Roles] r ON u.[roleId] = r.[id]
 )
-SELECT c.id, r.Name
-FROM cte c
-JOIN Roles r ON c.id = r.id;
+SELECT c.[id], r.[Name]
+FROM [cte] c
+JOIN [Roles] r ON c.[id] = r.[id];
