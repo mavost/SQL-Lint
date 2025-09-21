aed9d8a63fc3773874c1dc7410792c3be6588218
Markus von Steht <markus.von.steht@gmx.de>
2025-09-21 15:48:51 +0200

    sqlglot example

---
+++ scripts/testing.py
@@ -0,0 +1,13 @@
+from sqlglot import parse_one
+
+script = """
+WITH cte AS (
+    SELECT id, Name FROM Users u
+    JOIN Roles r ON u.roleId = r.id
+)
+SELECT c.id, r.Name
+FROM cte c
+JOIN Roles r ON c.id = r.id;
+"""
+
+print(repr(parse_one(script)))
